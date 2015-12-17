0b83b2270ccb7008de881cf4343cbf3398cce3e0
Richard Smith <richard.smith.67@gmail.com>
2015-12-17 10:34:27 +0000

    added transitionTo

--- psadmin/src/components/about/aboutPage.js
+++ psadmin/src/components/about/aboutPage.js
@@ -2,6 +2,17 @@
 var React = require('react');
 
 var About = React.createClass({
+
+    statics: {
+      willTransitionTo: function(transition, params, query, callback){
+        if(!confirm("Are you sure you want to move pages?")) {
+            transition.abort();
+        } else {
+            callback();
+        }
+      }
+    },
+
     render: function(){
         return (
           <div>
