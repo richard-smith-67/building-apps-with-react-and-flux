54a029c5e83847299cd9d47e60bab8da231dbff5
Richard Smith <richard.smith.67@gmail.com>
2015-12-17 14:49:36 +0000

    add author start

--- psadmin/src/components/authors/authorPage.js
+++ psadmin/src/components/authors/authorPage.js
@@ -5,6 +5,9 @@
 var React = require('react');
 var AuthorApi = require('../../api/authorApi');
 var AuthorList = require('./authorList');
+var Router = require('react-router');
+var Link = Router.Link;
+
 
 var AuthorPage = React.createClass({
 
@@ -26,6 +29,7 @@ var AuthorPage = React.createClass({
         return (
             <div>
                 <h1>Authors</h1>
+                <Link to="addAuthor" className="btn btn-default">Add Author</Link>
                 <AuthorList authors={this.state.authors}/>
             </div>
         );
---
+++ psadmin/src/components/authors/manageAuthorPage.js
@@ -0,0 +1,15 @@
+/**
+ * Created by richard.smith on 17/12/2015.
+ */
+"use strict";
+var React = require('react');
+
+var ManageAuthorPage = React.createClass({
+    render: function() {
+        return (
+            <h1>Manage Author</h1>
+        );
+    }
+});
+
+module.exports = ManageAuthorPage;
